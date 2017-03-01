a917b9506698ce0f626ff48f5e17a0c16829744b
hsb <1746534084@qq.com>
2017-03-01 16:01:22 +0800

    完成对购物车的添加和查询

---
+++ WingtipToys/Logic/ShoppingCartActions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WingtipToys.Models;
+
+namespace WingtipToys.Logic
+{
+    public class ShoppingCartActions:IDisposable
+    {
+        public string ShoppingCartId { get; set; }
+
+        private ProductContext db=new ProductContext();
+
+        public const string CartSessionKey = "CartId";
+
+        public void AddToCart(int id)
+        {
+            ShoppingCartId = GetCartId();
+            var cartItem = db.ShoppingCartItems.SingleOrDefault(
+                c => c.CartId == ShoppingCartId
+                     && c.ProductId == id);
+            if (cartItem == null)
+            {
+                cartItem = new CartItem()
+                {
+                    ItemId = Guid.NewGuid().ToString(),
+                    ProductId = id,
+                    CartId = ShoppingCartId,
+                    Product = db.Products.SingleOrDefault(p=>p.ProductID==id),
+                    DateCreated = DateTime.Now,
+                    Quantity = 1
+                };
+                db.ShoppingCartItems.Add(cartItem);
+            }
+            else
+            {
+                cartItem.Quantity++;
+            }
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (db!=null)
+            {
+                db.Dispose();
+                db = null;
+            }
+        }
+
+        public string GetCartId()
+        {
+            if (HttpContext.Current.Session[CartSessionKey] == null)
+            {
+                if (!string.IsNullOrEmpty(HttpContext.Current.User.Identity.Name))
+                {
+                    HttpContext.Current.Session[CartSessionKey] = HttpContext.Current.User.Identity.Name;
+                }
+                else
+                {
+                    HttpContext.Current.Session[CartSessionKey] = Guid.NewGuid().ToString();
+                }
+            }
+            return HttpContext.Current.Session[CartSessionKey].ToString();
+        }
+
+        public List<CartItem> GetCartItems()
+        {
+            ShoppingCartId = GetCartId();
+            return db.ShoppingCartItems.Where(c => c.CartId == ShoppingCartId).ToList();
+        }
+        public decimal GetTotal()
+        {
+            ShoppingCartId = GetCartId();
+            // Multiply product price by quantity of that product to get        
+            // the current price for each of those products in the cart.  
+            // Sum all product price totals to get the cart total.   
+            decimal? total = decimal.Zero;
+            total = (decimal?)(from cartItems in db.ShoppingCartItems
+                               where cartItems.CartId == ShoppingCartId
+                               select (int?)cartItems.Quantity *
+                               cartItems.Product.UnitPrice).Sum();
+            return total ?? decimal.Zero;
+        }
+    }
+}
--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+
 
 namespace WingtipToys.Models
 {
@@ -14,5 +11,8 @@ namespace WingtipToys.Models
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
+
     }
 }
